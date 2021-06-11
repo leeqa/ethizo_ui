88cb435c6cbb4b5ac5b02ee62e5e016f6753f71a
Mr.Nobody <mnobody565@gmail.com>
2021-06-11 15:53:09 +0500

    add alcohol, vaping, statuses

---
+++ app/src/main/java/com/gujranwala/ethizo_ui_muneeb/Fragments/alcohol/AlcoholCaffeineHydration.java
@@ -0,0 +1,26 @@
+package com.gujranwala.ethizo_ui_muneeb.Fragments.alcohol;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gujranwala.ethizo_ui_muneeb.R;
+
+
+public class AlcoholCaffeineHydration extends Fragment {
+
+    public AlcoholCaffeineHydration() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alcohol_caffeine_hydration, container, false);
+    }
+}
---
+++ app/src/main/java/com/gujranwala/ethizo_ui_muneeb/Fragments/statuses/Education.java
@@ -0,0 +1,26 @@
+package com.gujranwala.ethizo_ui_muneeb.Fragments.statuses;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gujranwala.ethizo_ui_muneeb.R;
+
+
+public class Education extends Fragment {
+
+    public Education() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_education, container, false);
+    }
+}
